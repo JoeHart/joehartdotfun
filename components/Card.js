5a07f717a67d321cbefd7d15b062cbc0c0d88d2a
Joe Hart <joehart42@outlook.com>
2020-05-20 21:32:09 +0100

    add game

---
+++ components/Card.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ButtonLink = styled.a`
+  display: block;
+  background: rgb(0, 113, 227);
+  border-radius: 0.5em;
+  font-size: 1em;
+  color: white;
+  padding: 0.75em;
+  font-weight: 400;
+  width: 100%;
+  cursor: pointer;
+  text-align: center;
+  &:hover {
+    background: rgb(0, 100, 200);
+  }
+`;
+
+const Image = styled.img`
+  width: 100%;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 0;
+`;
+
+export default function Card({ title, link, image, children }) {
+  return (
+    <Container>
+      <Image src={image} />
+      <Title>{title}</Title>
+      <div>{children}</div>
+      <ButtonLink href={link}>Play</ButtonLink>
+    </Container>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,163 +1,58 @@
 import Head from "next/head";
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import Card from "../components/Card";
+import Katamari from "../assets/Katamari.png";
+const GlobalStyle = createGlobalStyle`
+  html, 
+    body {
+    padding: 0;
+    margin: 0;
+    font-family: Helvetica, sans-serif;
+    }
 
+    * {
+    box-sizing: border-box;
+    }
+`;
+
+const PageContainer = styled.div`
+  max-width: 980px;
+  margin: 0 auto;
+`;
+
+const CardContainer = styled.div`
+  display: grid;
+  grid-gap: 1em;
+  grid-template-columns: 1fr 1fr 1fr;
+`;
+
+const Title = styled.div``;
 const Home = () => (
   <div className="container">
     <Head>
-      <title>Create Next App</title>
+      <title>Joe Hart Dot Fun</title>
       <link rel="icon" href="/favicon.ico" />
-      <link rel="stylesheet" href="https://use.typekit.net/rdi3nga.css"></link>
     </Head>
+    <GlobalStyle whiteColor />
 
     <main>
-      <h1 className="title">JoeHart.fun</h1>
+      <PageContainer>
+        <h1>joehartdotfun</h1>
+        <CardContainer>
+          <Card
+            title="Katamari Node Modules"
+            image={Katamari}
+            playLink="https://www.joehart.co.uk"
+          >
+            <p>
+              Fulfill your destiny as a node_modules folder and grow as big as
+              you can.
+            </p>
+          </Card>
+        </CardContainer>
+      </PageContainer>
     </main>
-
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 8rem;
-
-        font-family: sofia-pro, sans-serif;
-
-        font-weight: 900;
-        text-transform: uppercase;
-
-        font-style: normal;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
-        }
-      }
-    `}</style>
-
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: sofia-pro, sans-serif;
-      }
-
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
   </div>
 );
 
