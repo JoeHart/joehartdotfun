9422a53ae993c4f90ef827a9d4cd3d1e5f8b50a6
Joe Hart <joehart42@outlook.com>
2020-05-25 20:36:14 +0100

    Actually define an app

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import Router from "next/router";
 import withGA from "next-ga";
 
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
 export default withGA("UA-3930754-18", Router)(MyApp);
