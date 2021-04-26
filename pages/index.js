62be87efb7736557db94abc12992c6648c2bd4d3
Joe Hart <joehart42@outlook.com>
2021-04-26 21:58:47 +0100

    Add onclick event

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import SMS from "../assets/compressed/supermarketsolid.png";
 import DurhamDrive from "../assets/compressed/DurhamDrivesquare.png";
 import Footer from "../components/Footer";
 import EmailSignUp from "../components/EmailSignUp";
-
+import { usePlausible } from "next-plausible";
 const GlobalStyle = createGlobalStyle`
   html, 
     body {
@@ -65,61 +65,65 @@ export async function getStaticProps(context) {
   };
 }
 
-const Home = () => (
-  <div className="container">
-    <Head>
-      <title>Joe Hart Dot Fun</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <GlobalStyle whiteColor />
+const Home = () => {
+  const plausible = usePlausible();
+  return (
+    <div className="container">
+      <Head>
+        <title>Joe Hart Dot Fun</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <GlobalStyle whiteColor />
 
-    <main>
-      <PageContainer>
-        <Header>
-          <h1>joehartdotfun</h1>
-          <a href="https://twitter.com/joehart">
-            <TwitterLink src={TwitterLogo} alt="Twitter Profile" />
-          </a>
-        </Header>
+      <main>
+        <PageContainer>
+          <Header>
+            <h1>joehartdotfun</h1>
+            <a href="https://twitter.com/joehart">
+              <TwitterLink src={TwitterLogo} alt="Twitter Profile" />
+            </a>
+          </Header>
 
-        <CardContainer>
-          <Card
-            title="Durham Drive"
-            image={DurhamDrive}
-            buttonText="Play On Itch.io"
-            link="https://joehartfun.itch.io/durham-drive"
-          >
-            <p>
-              It's COVID19, so for some reason we're going to durham. Dodge the
-              police and get to the castle.
-            </p>
-          </Card>
-          <Card
-            title="Super Market Solid"
-            image={SMS}
-            link="/play/supermarketsolid/"
-          >
-            <p>
-              It's time to go to the shops, can you get the items, get to the
-              checkout and maintain social distancing?
-            </p>
-          </Card>
-          <Card
-            title="Katamari Node Modules"
-            image={Katamari}
-            link="/play/katamari/"
-          >
-            <p>
-              Fulfill your destiny as a node_modules folder and grow as big as
-              you can.
-            </p>
-          </Card>
-          <EmailSignUp />
-        </CardContainer>
-      </PageContainer>
-    </main>
-    <Footer />
-  </div>
-);
+          <CardContainer>
+            <Card
+              title="Durham Drive"
+              image={DurhamDrive}
+              buttonText="Play On Itch.io"
+              onClick={() => plausible("playDurhamDrive")}
+              link="https://joehartfun.itch.io/durham-drive"
+            >
+              <p>
+                It's COVID19, so for some reason we're going to durham. Dodge
+                the police and get to the castle.
+              </p>
+            </Card>
+            <Card
+              title="Super Market Solid"
+              image={SMS}
+              link="/play/supermarketsolid/"
+            >
+              <p>
+                It's time to go to the shops, can you get the items, get to the
+                checkout and maintain social distancing?
+              </p>
+            </Card>
+            <Card
+              title="Katamari Node Modules"
+              image={Katamari}
+              link="/play/katamari/"
+            >
+              <p>
+                Fulfill your destiny as a node_modules folder and grow as big as
+                you can.
+              </p>
+            </Card>
+            <EmailSignUp />
+          </CardContainer>
+        </PageContainer>
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
 export default Home;
